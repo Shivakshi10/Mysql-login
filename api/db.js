b9a30ec1fa8512970fc9c510363049a89e65eb85
Shivakshi10 <shivi.singh1485@gmail.com>
2023-06-27 22:27:50 +0530

    New change

--- api/db.js
+++ api/db.js
@@ -1,8 +1,12 @@
 import mysql from "mysql";
+import "dotenv/config";
+
+const SQL_PASSWORD = process.env.SQL_PASSWORD;
+const SQL_HOST = process.env.SQL_HOST;
 
 export const db = mysql.createConnection({
-    host:"127.0.0.1",
-    user:"root",
-    password:"Shapeofyou1",
-    database:"loginsys"
-})
+  host: SQL_HOST,
+  user: "root",
+  password: SQL_PASSWORD,
+  database: "loginsys",
+});
--- api/index.js
+++ api/index.js
@@ -1,19 +1,18 @@
-import express from "express"
-import customerRoutes from "./routes/customer.js"
-import authRoutes from "./routes/auth.js"
-import adminRoutes from "./routes/admin.js"
+import express from "express";
+import customerRoutes from "./routes/customer.js";
+import authRoutes from "./routes/auth.js";
+import adminRoutes from "./routes/admin.js";
+import cors from "cors";
 
+const app = express();
+app.use(cors());
 
-const app = express()
+app.use(express.json());
 
-app.use(express.json())
+app.use("/api/customer", customerRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
 
-app.use("/api/customer",customerRoutes);
-app.use("/api/auth",authRoutes); 
-app.use("/api/admin",adminRoutes);
-
-
-
-app.listen(8800,() =>{
-    console.log("Connected!");
-})
+app.listen(8800, () => {
+  console.log("Connected!");
+});
